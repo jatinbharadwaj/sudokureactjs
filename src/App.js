0da32afadf4b6d5b3b5d97fbb3f85aa40e7352f0
jatinbharadwaj <jitinrcks@gmail.com>
2020-11-27 19:51:16 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -5,33 +5,34 @@ import { solve, clear, undo} from './actions/grid';
 
 /* Application Container Component */
 const App = React.createClass({
+
 	componentDidMount(){
 		this.unsubscribe = this.props.store.subscribe(() => {
 			this.forceUpdate();
 		})
 	},
+
 	componentWillUnmount() {
 		this.unsubscribe();
 	},
+
 	render() {
 		const {store} = this.props;
 		const {grid, status} = store.getState();
 		const {isSolved, isEdited} = status;
 		return (
-			<div>
-				<button
+			<div><button
 					className='undo'
 					disabled={window.gridHistory && !window.gridHistory.length}
 					onClick={() => store.dispatch(undo())}
-				>
-					⤺ Undo
+				>⤺ Undo
 				</button>
+
 				<button
 					className='clear'
 					disabled={!isEdited}
 					onClick={() => store.dispatch(clear())}
-				>
-					⟲ Clear
+				>⟲ Clear
 				</button>
 
 				<Grid grid={grid} status={status} {...this.props}/>
@@ -49,17 +50,18 @@ const App = React.createClass({
 							alert('This Sudoku is NOT solvable');
 						}					
 					}}
-				>
-					Check
+				> Check
+
 				</button>
 				<button
 					className='solve'
 					onClick={() => store.dispatch(solve())}
 				>
 					Solve
-				</button>			
+				</button>	
+						
 				<div className='footnote'>
-					<p>by <a href='http://danialk.github.io/'> Danial Khosravi </a> </p>
+					<p>by <a href='https://github.com/jatinbharadwaj/'> Jatin Bhardwaj </a> </p>
 				</div>				
 			</div>
 
